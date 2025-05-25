16f84e9b30fd8fc97e8059e14c2465293bd98e0b
jorgemrt <jorge.martinez@sistemas.edu.bo>
2025-05-25 16:54:14 -0400

    segunda parte

--- DataLemur-Mrt/consultas.sql
+++ DataLemur-Mrt/consultas.sql
@@ -52,3 +52,148 @@ WHERE age BETWEEN 18 AND 22
   AND state IN ('Victoria', 'Tasmania', 'Queensland')
   AND gender != 'n/a'
   AND (customer_name LIKE 'A%' OR customer_name LIKE 'B%'); 
+--INTERMEDIATE SQL
+-- leeson 202 SUN ,AVG, COUNT
+-- 1.SQL COUNT Practice Exercise
+SELECT COUNT(*) FROM pharmacy_sales;
+-- 2. SQL SUM Practice Exercise
+SELECT COUNT(*), SUM(total_sales)
+FROM pharmacy_sales
+WHERE manufacturer = 'Pfizer'
+-- 3. SQL AVG Practice Exercise
+SELECT AVG(open)
+FROM stock_prices
+WHERE ticker='GOOG';
+-- 4. SQL MIN Practice Exercise: Microsoft Stock
+SELECT MIN(open)
+FROM stock_prices
+WHERE ticker='MSFT';
+--5. SQL MAX Practice Exercise: Tesla Stock
+SELECT MAX(open)
+FROM stock_prices
+WHERE ticker='NFLX';
+-- leeson 203 SQL GROUP BY
+-- 1.Easy SQL GROUP BY Practice Exercise
+SELECT ticker, MIN(open)
+FROM stock_prices
+GROUP BY ticker
+ORDER BY min DESC;
+-- 2. SQL GROUP BY Practice Exercise: Candidate Skills
+SELECT skill, COUNT(candidate_id) FROM candidates
+GROUP BY skill
+ORDER BY count DESC;
+-- leeson 204 SQL HAVING
+-- 1. SQL HAVING MIN  Practice Exercise
+SELECT ticker, MIN(open)
+FROM stock_prices
+GROUP BY ticker
+HAVING MIN(open) > 100;
+--2. SQL HAVING Practice Exercise
+SELECT candidate_id FROM candidates
+GROUP BY candidate_id
+HAVING COUNT(candidate_id) > 2;
+-- leeson 205 SQL distinct
+-- 1. SQL COUNT DISTINCT Practice Exercise
+SELECT category, COUNT(DISTINCT product)
+FROM product_spend
+GROUP BY category;
+-- leeson 206 SQL arithmetic
+-- 1. Pharmacy Analytics (Part 1)
+SELECT
+  drug,
+  total_sales - cogs AS total_profit
+FROM pharmacy_sales
+ORDER BY total_profit DESC
+LIMIT 3;
+-- 2. Cards Issued Difference
+SELECT 
+  card_name, 
+  MAX(issued_amount) - MIN(issued_amount) AS difference
+FROM monthly_cards_issued
+GROUP BY card_name
+ORDER BY difference DESC;
+-- 3. SQL Math Practice Exercise: Big-Mover Months
+SELECT ticker, COUNT(ticker)
+FROM stock_prices
+WHERE (close - open)/open > 0.10 OR (close - open)/open < -0.10
+GROUP BY ticker
+ORDER BY count DESC;
+-- leeson 207  SQL Math Functions
+-- 1. SQL CEIL Practice Exercise
+SELECT drug, CEIL(total_sales / units_sold) AS unit_cost
+FROM pharmacy_sales
+WHERE manufacturer = 'Merck'
+ORDER BY unit_cost;
+-- leeson 208 SQL DIVISION 
+-- 1. SQL DIVISION Practice Exercise
+-- pregunta bloqueada por subcripcion --
+-- leeson 209 SQL NULL
+-- 1. Unfinished Parts
+SELECT part, assembly_step
+FROM parts_assembly
+WHERE finish_date IS NULL;
+-- leeson 210 SQL CASE 
+-- 1. SQL Tutorial Lesson: Superheroes' Likes
+SELECT
+  actor,
+  character,
+  platform,
+  avg_likes,
+  CASE 
+    WHEN avg_likes >= 15000 THEN 'Super Likes'
+    WHEN avg_likes BETWEEN 5000 AND 14999 THEN 'Good Likes'
+    ELSE 'Low Likes'
+  END AS likes_category
+FROM marvel_avengers
+ORDER BY avg_likes DESC;
+-- 2. Laptop vs. Mobile Viewership
+SELECT 
+  SUM(CASE WHEN device_type = 'laptop' THEN 1 ELSE 0 END) AS laptop_views, 
+  SUM(CASE WHEN device_type IN ('tablet', 'phone') THEN 1 ELSE 0 END) AS mobile_views 
+FROM viewership;
+-- leeson 211 SQL JOIN
+-- 1. Easy SQL JOIN Practice Exercise
+SELECT *
+FROM trades
+INNER JOIN users
+  ON trades.user_id = users.user_id;
+  -- 2. Cities With Completed Trades
+  SELECT 
+  users.city, 
+  COUNT(trades.order_id) AS total_orders 
+FROM trades 
+INNER JOIN users 
+  ON trades.user_id = users.user_id 
+WHERE trades.status = 'Completed' 
+GROUP BY users.city 
+ORDER BY total_orders DESC
+LIMIT 3;
+-- 3. Page With No Likes
+SELECT p.page_id 
+FROM p LEFT JOIN page_likes 
+ON p.page_id=page_likes.page_id
+where page_likes.page_id is NULL;
+-- leeson 212 SQL Date Functions
+-- 1. Average Post Hiatus (Part 1)
+SELECT 
+	user_id, 
+    MAX(post_date::DATE) - MIN(post_date::DATE) AS days_between
+FROM posts
+WHERE DATE_PART('year', post_date::DATE) = 2021 
+GROUP BY user_id
+HAVING COUNT(post_id)>1;
+-- 2. Second Day Confirmation
+SELECT DISTINCT user_id
+FROM emails 
+INNER JOIN texts
+  ON emails.email_id = texts.email_id
+WHERE texts.action_date = emails.signup_date + INTERVAL '1 day'
+  AND texts.signup_action = 'Confirmed';
+  -- 3. Average Post Hiatus (Part 1)
+  SELECT 
+	user_id, 
+    MAX(post_date::DATE) - MIN(post_date::DATE) AS days_between
+FROM posts
+WHERE DATE_PART('year', post_date::DATE) = 2021 
+GROUP BY user_id
+HAVING COUNT(post_id)>1;
